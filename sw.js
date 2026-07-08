7da4c084d79cca802be85152a0e4d009af84e47b
drakifyildiz <16301125037@ogr.bozok.edu.tr>
2026-07-08 06:19:04 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'mod-app-v1';
+const urlsToCache = [
+  '/',
+  '/login.html',
+  '/dashboard.html',
+  '/committee.html',
+  '/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
